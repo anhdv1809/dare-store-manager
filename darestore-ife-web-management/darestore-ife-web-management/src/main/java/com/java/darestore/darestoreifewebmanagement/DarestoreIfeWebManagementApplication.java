3b543e088e734386583e36371d63c1b5bd6f60d1
Ho Dac Vinh An <anhdv112233@gmail.com>
2018-03-14 17:26:25 +0700

    DARE-000 : Initialize

---
+++ darestore-ife-web-management/darestore-ife-web-management/src/main/java/com/java/darestore/darestoreifewebmanagement/DarestoreIfeWebManagementApplication.java
@@ -0,0 +1,12 @@
+package com.java.darestore.darestoreifewebmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarestoreIfeWebManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarestoreIfeWebManagementApplication.class, args);
+	}
+}
